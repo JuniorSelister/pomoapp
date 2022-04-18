6b5fae331904925f4b72979b8aea9856b5573539
PedroSelister <pedro.selister@sap.com>
2022-04-18 19:43:04 -0300

    Uploading BtnStart and Index changes

--- src/components/BtnStart.jsx
+++ src/components/BtnStart.jsx
@@ -5,19 +5,9 @@ import './BtnStyle.css'
 // eslint-disable-next-line
 export default props => {
 
-  const [time] = useState();
-
-  function onStart() {
-    setInterval(() => {
-      if (time > 0) {
-        console.log(time = time - 1)
-      }
-    }, 1000)
-  }
-
   return (
     <div className="btn" id='start'>
-      <span className='noselect' onClick={onStart()}>Start</span>
+      <span className='noselect'>Start</span>
     </div>
   )
 }
--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -8,14 +8,24 @@ import './Index.css'
 // eslint-disable-next-line
 export default (props) => {
   
-  const [timerDefault, setTimer] = useState(30);
+  const [timerDefault, setTimer] = useState(10);
+
+  function onStart(e) {
+    setInterval(() => {
+      if (timerDefault > 0) {
+        setTimer(e.target.value - 1)
+      } else {
+        alert("Time's up!");
+      };
+    }, 1000)
+  }
 
   return (
     
     <div className='container'>
 
       <div className='timer'>
-        <h2>{timerDefault}</h2>
+        <h2> { timerDefault }</h2>
         <BtnStart />
         <BtnPause />
       </div>
